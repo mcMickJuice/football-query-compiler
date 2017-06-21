9b6a99717a8cd585af3559aa506cc5bf742759d9
Mike Joyce <mikejoyce19@gmail.com>
2017-06-20 21:43:59 -0500

    wired up examples. got stuff to show up on page

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -4,6 +4,8 @@ module.exports = {
   entry: path.resolve(__dirname, './src/main.js'),
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist'
+  },
+  devtool: 'source-map'
 }
