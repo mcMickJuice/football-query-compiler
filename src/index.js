aa9b692d5403141605eeb7713c26843054f1b0b0
Mike Joyce <mikejoyce19@gmail.com>
2017-06-25 20:10:44 -0500

    removed example dir, moved to other repo. added index

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const parser = require('./parser')
+const tokenizer = require('./tokenizer')
+const traverser = require('./traverser')
+
+
+module.exports.parser = parser;
+module.exports.tokenizer = tokenizer;
+module.exports.traverser = traverser;
